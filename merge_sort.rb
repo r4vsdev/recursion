147e1d318b8a62091ecb58c0249d5bedc44d9e3f
r4vsdev <r4vsdev@gmail.com>
2023-03-18 18:04:10 -0300

    Add the basic recursion structure to divide the array

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,5 +1,29 @@
-def merge_sort(array, sorted = [])
+def min (a, b)
+  a < b ? a : b
+end
+
+def merge_sort(array)
+  if array.size <= 2
+    p array
+    a = array.take(1)
+    b = array.drop(1)
+  else
+    a = merge_sort(array.take(array.size / 2))
+    b = merge_sort(array.drop(array.size / 2))
+  end
+  # p a
+  # p b
+  sorted = []
+  a.each do |el_a|
+    b.each do |el_b|
+      el_a < el_b ? sorted.push(el_a, el_b) : sorted.push(el_b, el_a)
+    end
+  end
+
   
+
+  p "sorted = #{sorted}"
+  return sorted
 end
 
 merge_sort([8, 4, 5, 6])
