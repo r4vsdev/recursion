9a39e4f7dd3f9a4e941cea27a0cce74c1fae50db
r4vsdev <r4vsdev@gmail.com>
2023-03-20 13:51:32 -0300

    Remove debugger entry points

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,11 +1,8 @@
-def min(a, b)
-  a < b ? a : b
-end
+# frozen_string_literal: true
 
 # sort the array using the merge sort algorithm
 def merge_sort(array)
   if array.size <= 2
-    # p array
     a = array.take(1)
     b = array.drop(1)
   else
@@ -14,38 +11,20 @@ def merge_sort(array)
   end
 
   sorted = []
-  # a.each do |el_a|
-  #   b.each do |el_b|
-
-  #   end
-  # end
-
   while a != [] || b != []
     if a == []
       sorted.push(b.shift)
-      # puts "next"
       next
     elsif b == []
       sorted.push(a.shift)
-      # puts "next"
       next
     end
-    # p [a.min, b.min]
     if a.min < b.min
-      # puts "push #{a.min} to sort"
       sorted.push(a.delete(a.min))
     else
-      # puts "push #{b.min} to sort"
       sorted.push(b.delete(b.min))
     end
-    # p [a, b]
   end
-  # puts "exiting the while"
 
-  p "sorted = #{sorted}"
-  return sorted
+  sorted
 end
-
-# merge_sort([8, 4])
-# merge_sort([8, 4, 5, 6])
-merge_sort([8, 4, 5, 6, 2, 7, 1, 9])
